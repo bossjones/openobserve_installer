00a04ec019e9484cb76a7c86819dc7324935e8fd
Malcolm Jones <bossjones@theblacktonystark.com>
2024-12-09 23:26:17 -0500

    build(syslog_log_generator): add tools and env var for remote logging setup

--- containers/syslog_log_generator/generate_logs.sh
+++ containers/syslog_log_generator/generate_logs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python3 syslog_gen.py --host 127.0.0.1 --port 5514 --file sample_logs.txt --count 100000
+# python3 syslog_gen.py --host 127.0.0.1 --port 5514 --file sample_logs.txt --count 100000
+set -x
+python3 syslog_gen.py --host ${REMOTE_HOSTNAME_LOGS} --port 5514 --file sample_logs.txt --count 100000
